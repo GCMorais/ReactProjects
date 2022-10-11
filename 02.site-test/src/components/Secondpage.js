49347204bb0d82e2a60648d4023e57ec70b6bdfc
GUilherme <guilhermeclarodemorais@gmail.com>
2022-10-11 17:10:25 -0300

    SecondPage add

--- 02.site-test/src/components/Secondpage.js
+++ 02.site-test/src/components/Secondpage.js
@@ -4,9 +4,23 @@ function SecondPage(){
     return (
         <div className="ContainerPost">
             <section className='secondArea'>
-                <h3>Oodsaodsosd</h3>
+                <div className='area1'>
+                    <h2>What makes us a special agency</h2>
+                    <button>Let's discuss your ideas</button>
+                </div>
+                <div className='area2'>
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et odio risus. Quisque aliquam fermentum ligula, at elementum metus gravida ut. 
+                    </h3>
+                </div>
             </section>
+            <div class="custom-shape-divider-top-1665518237">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
+</div>
         </div>
+        
     )
 }
 
