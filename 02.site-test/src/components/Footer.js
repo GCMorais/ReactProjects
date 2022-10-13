f5a95e478dbec1ce0640e727f31bea18103896df
GUilherme <guilhermeclarodemorais@gmail.com>
2022-10-13 16:54:32 -0300

    Updates

--- 02.site-test/src/components/Footer.js
+++ 02.site-test/src/components/Footer.js
@@ -3,7 +3,8 @@ import './Footer.css';
 function Footer(){
     return(
         <div className='ContainerFooter'>
-            
+            <h1>Example site by Guilherme C.Morais</h1>
+            <p>&copy; 2022 Example.com</p>
         </div>
     )
 }
