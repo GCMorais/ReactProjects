d8e43ed298920d9f81f26a232f355eaa79d7a81d
GUilherme <guilhermeclarodemorais@gmail.com>
2022-10-24 15:05:21 -0300

    Card add and input styled

--- 03.explorerlab/src/components/main.js
+++ 03.explorerlab/src/components/main.js
@@ -5,6 +5,7 @@ function Card(){
         <div className="Container">
             <div className="pay-box">
                 <div className="input">
+                    <img src="./logo.svg"></img>
                     <div className="input-p">
                         <p>Número do cartão</p>
                         <input></input>
@@ -26,7 +27,28 @@ function Card(){
                     <button>Adicionar cartão</button>
                 </div>
                 <div className="card">
-
+                    <div className="card-box">
+                        <div className="first-icons">
+                            <img src="./cc-icon.svg"></img>
+                            <img src="./cc-visa.svg"></img>
+                        </div>
+                        <div className="card-infos">
+                            <h1>1234 5678 9012 3456</h1>
+                            <p>Nome do titular</p>
+                            <h2>Fulano da Silva</h2>
+                        </div>
+                        <div className="card-infos-box">
+                            <div className="inf-area">
+                                <p>Expiração</p>
+                                <h2>02/32</h2>
+                            </div>
+                            <div className="inf-area">
+                                <p>CVC</p>
+                                <h2>123</h2>
+                            </div>
+                            <img src="./cc-chip.svg"></img>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
